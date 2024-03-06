d3412df9d82a430ccb5b53977d2ac3368bb770ce
TCreagh0098 <125223853+TCreagh0098@users.noreply.github.com>
2024-03-07 12:06:40 +1300

    Add files via upload

---
+++ 01_list_demo.py
@@ -0,0 +1,11 @@
+some_colors = [
+    ['alice blue', '19', 'black'],
+    ['antique white', '20', 'black'],
+    ['AntiqueWhite1', '20', 'black'],
+    ['AntiqueWhite2', '19', 'black'],
+    ['AntiqueWhite3', '16', 'black'],
+    ['AntiqueWhite4', '11', 'black']
+]
+
+for item in some_colors:
+    print(item[0])
